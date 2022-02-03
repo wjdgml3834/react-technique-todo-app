f465abed8fdd634fc366b649b9a7d5182097b037
junghee.kim <wjdgml3834@naver.com>
2022-02-03 13:19:27 +0900

    💄  TodoTemplate 스타일 추가

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -1,10 +1,33 @@
+import styled from "@emotion/styled";
+
 const TodoTemplate = ({ children }) => {
   return (
-    <div>
-      <h1>일정 관리</h1>
-      <div>{children}</div>
-    </div>
+    <TodoCont>
+      <Title>일정 관리</Title>
+      <Content>{children}</Content>
+    </TodoCont>
   );
 };
+
+const TodoCont = styled.div`
+  width: 512px;
+  border-radius: 4px;
+  margin: 0 auto;
+  margin-top: 6rem;
+  overflow: hidden;
+`;
+const Title = styled.h1`
+  background: #fcd683;
+  color: white;
+  height: 4rem;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Content = styled.section`
+  background: white;
+`;
 
 export default TodoTemplate;
