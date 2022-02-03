a9d2d81e414827d73e255bbc82437ca6fc544444
junghee.kim <wjdgml3834@naver.com>
2022-02-03 15:54:04 +0900

    💄  TodoInsert 컴포넌트 스타일 적용

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,14 +1,49 @@
 import { MdAdd } from "react-icons/md";
+import styled from "@emotion/styled";
 
 const TodoInsert = () => {
   return (
-    <form>
-      <input placeholder="할 일을 입력하세요" />
-      <button type="submit">
+    <Cont>
+      <Input placeholder="할 일을 입력하세요" />
+      <SubmitBtn type="submit">
         <MdAdd />
-      </button>
-    </form>
+      </SubmitBtn>
+    </Cont>
   );
 };
 
 export default TodoInsert;
+
+const Cont = styled.form`
+  display: flex;
+  background: #495057;
+`;
+const Input = styled.input`
+  background: none;
+  outline: none;
+  border: none;
+  padding: 0.5rem;
+  font-size: 1.125rem;
+  line-height: 1.5;
+  color: white;
+  &::placeholder {
+    color: #dee2e6;
+  }
+  flex: 1;
+`;
+
+const SubmitBtn = styled.button`
+  background: none;
+  outline: none;
+  border: none;
+  background: #868e96;
+  color: white;
+  padding: 0 1rem;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  transition: 0.1s background ease-in;
+  &:hover {
+    background: #adb5bd;
+  }
+`;
