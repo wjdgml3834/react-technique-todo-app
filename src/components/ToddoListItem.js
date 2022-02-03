d3966f5a9cab53c21903d44b2003d06b79aa1d0a
junghee.kim <wjdgml3834@naver.com>
2022-02-03 20:01:25 +0900

    💄  TodoList ,TodoListItem 스타일 적용

--- src/components/ToddoListItem.js
+++ src/components/ToddoListItem.js
@@ -1,17 +1,58 @@
+import styled from "@emotion/styled";
 import { MdCheckBoxOutlineBlank, MdRemoveCircleOutline } from "react-icons/md";
 
 const TodoListItem = () => {
   return (
-    <div>
-      <div>
+    <ItemCont>
+      <CheckBoxCont className="">
         <MdCheckBoxOutlineBlank />
-        <div>할일</div>
-      </div>
-      <div>
+        <p>할일</p>
+      </CheckBoxCont>
+      <RemoveCont>
         <MdRemoveCircleOutline />
-      </div>
-    </div>
+      </RemoveCont>
+    </ItemCont>
   );
 };
+
+const ItemCont = styled.div`
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  &:nth-child(even) {
+    background: #f8f9fa;
+  }
+`;
+
+const CheckBoxCont = styled.button`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  svg {
+    font-size: 1.5rem;
+  }
+  p {
+    margin-left: 0.5rem;
+  }
+  &.checked {
+    svg {
+      color: #22b8cf;
+    }
+    p {
+      color: #adb5bd;
+      text-decoration: line-through;
+    }
+  }
+`;
+
+const RemoveCont = styled.button`
+  display: flex;
+  align-items: center;
+  font-size: 1.5rem;
+  color: #ff6b6b;
+  &:hover {
+    color: #ff8787;
+  }
+`;
 
 export default TodoListItem;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,20 @@
 import TodoListItem from "./ToddoListItem";
+import styled from "@emotion/styled";
 
 const TodoList = () => {
   return (
-    <div>
+    <Cont>
       <TodoListItem />
       <TodoListItem />
       <TodoListItem />
-    </div>
+    </Cont>
   );
 };
+
+const Cont = styled.div`
+  min-height: 320px;
+  max-height: 513px;
+  overflow-y: auto;
+`;
 
 export default TodoList;
